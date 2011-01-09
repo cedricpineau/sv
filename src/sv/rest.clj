282a3c46b540b183c6a1daa5611e9a5ae49c25cd
Cédric Pineau <cedric.pineau@gmail.com>
2011-01-09 22:42:30 +0100

    ajout d'une sorte de login (seul l'existence de l'email est vérifiée pour le moment), et d'une page d'accueil une fois authentifiée : le dashboard

--- src/sv/rest.clj
+++ src/sv/rest.clj
@@ -1,7 +1,14 @@
 (ns sv.rest
+  (:require (sv [data :as data]))
   (:use compojure.core)
+  (:use ring.util.response)
   (:require [compojure.route :as route]))
 
 (defroutes sv
   (route/files "/" {:root "web"})
-  (route/not-found "Page not found"))
+  (POST "/login" {params :params} 
+    ; (str "POST email=" (params "email") " params=" params))
+    (if (data/find-user-by-email (params "email"))
+    (file-response "./web/private/dashboard.html")
+    (file-response "./web/index.html")))
+  (route/not-found "Page not found :-/"))
