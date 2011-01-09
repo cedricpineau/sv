3975287ee98a6b5d7797821d3411366762d26dfc
Cédric Pineau <cedric.pineau@gmail.com>
2011-01-09 22:41:07 +0100

    ajout du répertoire de conf dans le path

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject sv "0.1.0"
   :description "The functional SV"
   :url "http://sv.com"
+
+  :main sv.main
+  :resources-path "conf"
+  
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [fleetdb "0.3.1"]
                  [compojure "0.5.3"]
-                 [ring/ring-jetty-adapter "0.3.1"]]
-  :main sv.main)
+                 [ring/ring-jetty-adapter "0.3.1"]])
