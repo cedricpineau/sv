ff596b304aa339a5286a912eee6d668b3e6f166c
Cédric Pineau <cedric.pineau@gmail.com>
2011-01-09 22:17:48 +0100

    spécification du main pour pouvoir lancer le projet avec 'lein run'

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject sv "0.1.0"
   :description "The functional SV"
+  :url "http://sv.com"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [fleetdb "0.3.1"]
                  [compojure "0.5.3"]
-                 [ring/ring-jetty-adapter "0.3.1"]])
+                 [ring/ring-jetty-adapter "0.3.1"]]
+  :main sv.main)
